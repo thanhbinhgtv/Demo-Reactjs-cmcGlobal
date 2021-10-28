79403aac05e08a75d17c82c070903b99bd5b6aa9
Thanh Binh <thanhbinhgtv@gmail.com>
2021-10-28 17:00:17 +0700

    28/10/2021

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ function App() {
   const hello = 'Hello guys';
   const [count, setCount] = useState(25);
 
-  function test(e) {
+  const changeCount = (e) => {
     console.log(e.target.value);
+    setCount(e.target.value);
   }
 
   return (
     <>
       <div className='App'>
         <h1>This a React App</h1>
-        <Person onClick={test} name='Binh' age={count} hello={hello}> (Developer)</Person>
+        <Person changeCount={changeCount} name='Binh' age={count} hello={hello}> (Developer)</Person>
         <button onClick={() => setCount(count+1)}>Count Age</button>
       </div>
     </>
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonDate from "./PersonDate";
-import './Person.css'
+import "./Person.css";
 import Card from "./Card";
 
-const Person = (props) => {
+const Person = ({ hello, changeCount, age, children, name }) => {
+  const [toggle, setToggle] = useState(true);
+  const [text, setText] = useState(hello);
+
+  const changeToggle = () => {
+    setToggle(!toggle);
+    console.log(toggle);
+  };
+
+  const changeText = (e) => {
+    e.preventDefault();
+    if (text === "Hello After") {
+      setText("Hello Before");
+      console.log(text);
+    } else {
+      setText("Hello After");
+      console.log(text);
+    }
+  };
 
   return (
     <>
-    <button onClick={props.onClick('ss')}>Click Me</button>
-
-    <Card>
-      <PersonDate hello={props.hello}></PersonDate>
-      <p className="person">This a Persion</p>
-      <p className="abc">
-        This name : {props.name} - This age : {props.age} {props.children}
-      </p>
-    </Card>
+      <form onSubmit={changeText}>
+        <button onClick={() => changeToggle()}>Toggle</button>
+        <button onClick={() => changeText()}>Change Text</button>
+        <button type='submit'>Submit</button>
+        <input type="number" onChange={changeCount} value={age}></input>
+      </form>
+      {toggle === true ? (
+        <Card>
+          <PersonDate hello={text}></PersonDate>
+          <p className="person">This a Persion</p>
+          <p className="abc">
+            This name : {name} - This age : {age} {children}
+          </p>
+        </Card>
+      ) : null}
     </>
   );
 };
