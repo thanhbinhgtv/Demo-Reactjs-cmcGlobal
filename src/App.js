7e5f5075d79b67a914f0fd7b38dc63d56b60321d
Thanh Binh <thanhbinhgtv@gmail.com>
2021-10-29 17:28:16 +0700

    29/10/2021

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ function App() {
   const hello = 'Hello guys';
   const [count, setCount] = useState(25);
 
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState([]);
+
   const changeCount = (e) => {
     console.log(e.target.value);
     setCount(e.target.value);
   }
+
+  const Add = () => {
+    setJobs(prev => [...prev, job]);
+    setJob('');
+  }
+
 
   return (
     <>
@@ -17,6 +26,16 @@ function App() {
         <h1>This a React App</h1>
         <Person changeCount={changeCount} name='Binh' age={count} hello={hello}> (Developer)</Person>
         <button onClick={() => setCount(count+1)}>Count Age</button>
+
+        <input value={job} onChange={e =>setJob(e.target.value)} />
+        <button onClick={Add}>Add</button>
+
+        <ul>
+          {jobs.map((job, index) => (
+            <li key={index}>{job}</li>
+          ))
+          }
+        </ul>
       </div>
     </>
   );
